b47306755b24fff5289468c1761d1876662c3ac8
sliwati11 <salwamerbouha@gmail.com>
2019-07-29 02:55:36 +0200

    commit

--- src/server/server.js
+++ src/server/server.js
@@ -9,10 +9,42 @@ const client = new Client({
   database : 'postgres' 
 });
 
-client.connect()
-.then( () =>  console.log("Connected Successfuly"))
-.then( () => client.query(" INSERT INTO test VALUES ($1)", ["sahar"]))
-.then( () => client.query("select * from test where name =$1",["sahar"] ))
-.then( results => console.table(results.rows) )
-.catch( e => console.log(e))
-.finally( () => client.end())
+
+
+
+
+execute()
+
+
+async function execute(){
+  try{
+    await client.connect()
+    console.log("Connected successfully")
+    //const results = await client.query("update test set firstname=$1, lastname=$2 where name =$3",['sdf','fete','Guerta'])
+    const results = await client.query("select * from test")
+    console.log("Updated successfully")
+    console.table(results.rows)
+    
+  }catch(ex){
+    const results2 = await client.query("select * from test;")
+    console.table(results2.rows)
+    console.log(
+      'Something wrong happened $1,["ex"]')
+  }
+  finally{
+    await client.end()
+    console.log("Client disconnected successfully")
+  }
+  
+}
+
+function execute_with_promise(){
+
+  client.connect()
+  .then( () =>  console.log("Connected Successfuly"))
+  /* .then( () => client.query(" INSERT INTO account VALUES ($1)", ["sahar"])) */
+  .then( () => client.query("select * from test where name =$1",["sahar"] ))
+  .then( results => console.table(results.rows) )
+  .catch( e => console.log(e))
+  .finally( () => client.end()) 
+}
